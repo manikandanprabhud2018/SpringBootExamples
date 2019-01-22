f8dbc5fc2144b3611dcfcc2514f868418574bb93
user <user@DESKTOP-AGBN6UV>
2019-01-22 22:37:06 +0530

    comments added

--- springbootdemo/src/main/java/com/manik/springboot/demo/SpringBootDemoApplication.java
+++ springbootdemo/src/main/java/com/manik/springboot/demo/SpringBootDemoApplication.java
@@ -3,6 +3,11 @@ package com.manik.springboot.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author user
+ *
+ */
 @SpringBootApplication 
 public class SpringBootDemoApplication {
 
